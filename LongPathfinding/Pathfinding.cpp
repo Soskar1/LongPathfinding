4ee2aee4358a9751efb41e724398d228f5321d64
Soskar1 <89640069+Soskar1@users.noreply.github.com>
2023-05-31 17:46:07 +0300

    Accurate Longest Path

--- LongPathfinding/Pathfinding.cpp
+++ LongPathfinding/Pathfinding.cpp
@@ -5,13 +5,13 @@
 #include <set>
 
 namespace Pathfinding {
-	struct GraphInfo {
+	struct Path {
+		int currentCost;
 		std::vector<bool> visited;
 		std::vector<int> distances;
 		std::map<Graphs::Node*, Graphs::Node*> cameFrom;
-		int currentCost;
 
-		GraphInfo(std::vector<Graphs::Node*> nodes) {
+		Path(std::vector<Graphs::Node*> nodes) {
 			visited = std::vector<bool>(nodes.size(), false);
 			distances = std::vector<int>(nodes.size(), INT_MIN);
 			cameFrom = std::map<Graphs::Node*, Graphs::Node*>();
@@ -20,14 +20,14 @@ namespace Pathfinding {
 	};
 
 	struct NodeCompare {
-		bool operator()(const std::pair<Graphs::Node*, GraphInfo>& node1, const std::pair<Graphs::Node*, GraphInfo>& node2) const {
+		bool operator()(const std::pair<Graphs::Node*, Path>& node1, const std::pair<Graphs::Node*, Path>& node2) const {
 			return node1.second.currentCost < node2.second.currentCost;
 		}
 	};
 
 	struct PathCompare {
-		bool operator()(const GraphInfo& path1, const GraphInfo& path2) const {
-			return path1.currentCost < path2.currentCost;
+		bool operator()(const Path& path1, const Path& path2) const {
+			return path1.currentCost > path2.currentCost;
 		}
 	};
 
@@ -66,51 +66,53 @@ namespace Pathfinding {
 		using namespace Graphs;
 
 		std::vector<Node*> graphNodes = graph.GetNodes();
-		std::set<GraphInfo, PathCompare> paths;
+		std::set<Path, PathCompare> paths;
 
-		GraphInfo graphInfo(graphNodes);
+		Path graphInfo(graphNodes);
 		graphInfo.distances[startNode->GetID()] = 0;
 
-		std::priority_queue<std::pair<Node*, GraphInfo>, std::vector<std::pair<Node*, GraphInfo>>, NodeCompare> queue;
+		std::priority_queue<std::pair<Node*, Path>, std::vector<std::pair<Node*, Path>>, NodeCompare> queue;
 
 		queue.push(std::make_pair(startNode, graphInfo));
 
 		while (!queue.empty()) {
-			auto node = queue.top();
+			auto pathInfo = queue.top();
 			queue.pop();
 
-			if (node.first == endNode) {
-				paths.insert(node.second);
+			if (pathInfo.first == endNode) {
+				paths.insert(pathInfo.second);
 				continue;
 			}
 
-			GraphInfo graphInfo = node.second;
+			Node* node = pathInfo.first;
+			Path path = pathInfo.second;
 
-			auto neighbours = node.first->GetOutEdges();
+			path.visited[node->GetID()] = true;
+
+			auto neighbours = node->GetOutEdges();
 
 			for (auto neighbour : neighbours) {
 				Node* neighbourNode = neighbour.first;
 				int weight = neighbour.second;
 
-				if (graphInfo.visited[neighbourNode->GetID()] && graph.Connected(neighbourNode, node.first))
+				if (path.visited[neighbourNode->GetID()] && graph.Connected(neighbourNode, node))
 					continue;
 
-				int totalCost = graphInfo.distances[node.first->GetID()] + weight;
+				int totalCost = path.distances[node->GetID()] + weight;
 
-				if (totalCost > graphInfo.distances[neighbourNode->GetID()]) {
-					graphInfo.distances[neighbourNode->GetID()] = totalCost;
+				if (totalCost > path.distances[neighbourNode->GetID()]) {
+					path.distances[neighbourNode->GetID()] = totalCost;
+					path.currentCost = totalCost;
 
-					graphInfo.cameFrom[neighbourNode] = node.first;
-					queue.push(std::make_pair(neighbourNode, graphInfo));
+					path.cameFrom[neighbourNode] = node;
+					queue.push(std::make_pair(neighbourNode, path));
 				}
 			}
-
-			graphInfo.visited[node.first->GetID()] = true;
 		}
 
 		if (paths.size() != 0) {
-			auto graphInfo = *paths.begin();
-			return ConstructPath(graphInfo.cameFrom, endNode);
+			auto path = *paths.begin();
+			return ConstructPath(path.cameFrom, endNode);
 		}
 		else {
 			return std::list<Node*>();
